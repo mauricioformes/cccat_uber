546ff354e27aaf8f1d3156551b22eecf27d83043
Mauricio Pires Formes <mauricio_mama_nh@hotmail.com>
2023-10-31 22:30:43 -0300

    ajustes nos testes

--- backend/ride/test/unit/Driver.test.ts
+++ backend/ride/test/unit/Driver.test.ts
@@ -1,23 +1,23 @@
 import Driver from "../../src/domain/Driver";
 
 test("Deve criar um motorista", function () {
-    const driver = Driver.create("John Doe", "john.doe@gmail.com", "83432616074", "AAA9999");
+    const driver = Driver.create("Maurício Formes", "mauricioformes@hotmail.com", "83432616074", "AAA9999");
     expect(driver.driverId).toBeDefined();
-    expect(driver.name).toBe("John Doe",);
-    expect(driver.email.value).toBe("john.doe@gmail.com");
+    expect(driver.name).toBe("Maurício Formes",);
+    expect(driver.email.value).toBe("mauricioformes@hotmail.com");
     expect(driver.document.value).toBe("83432616074");
     expect(driver.carPlate.value).toBe("AAA9999");
 });
 
 
 test("Não deve criar um motorista com CPF inválido", function () {
-    expect(() => Driver.create("John Doe", "john.doe@gmail.com", "83432616076", "AAA9999")).toThrow(new Error("CPF Inválido"));
+    expect(() => Driver.create("Maurício Formes", "mauricioformes@hotmail.com", "83432616076", "AAA9999")).toThrow(new Error("CPF Inválido"));
 });
 
 test("Não deve criar um motorista com email inválido", function () {
-    expect(() => Driver.create("John Doe", "john.doe@gmail", "83432616074", "AAA9999")).toThrow(new Error("Email Inválido"));
+    expect(() => Driver.create("Maurício Formes", "emailerrado@errado", "83432616074", "AAA9999")).toThrow(new Error("Email Inválido"));
 });
 
 test("Não deve criar um motorista com placa do carro inválido", function () {
-    expect(() => Driver.create("John Doe", "john.doe@gmail.com", "83432616074", "AAA999")).toThrow(new Error("Placa Inválida"));
+    expect(() => Driver.create("Maurício Formes", "mauricioformes@hotmail.com", "83432616074", "AAA999")).toThrow(new Error("Placa Inválida"));
 });
--- backend/ride/test/unit/Email.test.ts
+++ backend/ride/test/unit/Email.test.ts
@@ -1,12 +1,12 @@
 import Email from "../../src/domain/Email";
 
 test("Deve validar o email válido", function(){
-    const email = new Email("john.doe@gmail.com");
+    const email = new Email("mauricioformes@hotmail.com");
     expect(email).toBeTruthy();
 });
 
 
 test("Não deve validar o email inválido", function(){
-    const email = "john.doe@gmail";
+    const email = "umastringqualquer241414135";
     expect(() => new Email(email)).toThrow(new Error("Email Inválido"));
 })
