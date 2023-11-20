2fe6ac8e3641b01dd8e6986670dcca9356c449c9
Mauricio Pires Formes <mauricio_mama_nh@hotmail.com>
2023-11-20 13:13:45 -0300

    asd

--- backend/ride/test/unit/Passenger.test.ts
+++ backend/ride/test/unit/Passenger.test.ts
@@ -1,9 +1,9 @@
 import Passenger from "../../src/domain/passenger/Passenger";
 
 test("Deve criar um passageiro", function () {
-    const passenger = Passenger.create("John Doe", "john.doe@gmail.com", "83432616074");
+    const passenger = Passenger.create("Maurício Pires Formes", "john.doe@gmail.com", "83432616074");
     expect(passenger.passengerId).toBeDefined();
-    expect(passenger.name).toBe("John Doe",);
+    expect(passenger.name).toBe("Maurício Pires Formes",);
     expect(passenger.email.value).toBe("john.doe@gmail.com");
     expect(passenger.document.value).toBe("83432616074");
 
@@ -11,9 +11,9 @@ test("Deve criar um passageiro", function () {
 
 
 test("Não deve criar um passageiro com CPF inválido", function () {
-    expect(() => Passenger.create("John Doe", "john.doe@gmail.com", "83432616076")).toThrow(new Error("CPF Inválido"));
+    expect(() => Passenger.create("Maurício Pires Formes", "john.doe@gmail.com", "83432616076")).toThrow(new Error("CPF Inválido"));
 });
 
 test("Não deve criar um passageiro com email inválido", function () {
-    expect(() => Passenger.create("John Doe", "john.doe@gmail", "83432616074")).toThrow(new Error("Email Inválido"));
+    expect(() => Passenger.create("Maurício Pires Formes", "john.doe@gmail", "83432616074")).toThrow(new Error("Email Inválido"));
 });
